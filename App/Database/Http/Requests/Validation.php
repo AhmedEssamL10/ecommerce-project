afc9b90dd1b8efb9b0286e452832bf6412c7cccb
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-06-26 05:32:54 +0300

    register validation

---
+++ App/Database/Http/Requests/Validation.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Database\Http\Requests;
+// this class validate on inputs and return errors if exists
+class validation
+{
+    private string $input;
+    private $value;
+    private array $errors = [];
+
+    /**
+     * Get the value of input
+     */
+    public function getInput()
+    {
+        return $this->input;
+    }
+
+    /**
+     * Set the value of input
+     *
+     * @return  self
+     */
+    public function setInput($input)
+    {
+        $this->input = $input;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of value
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set the value of value
+     *
+     * @return  self
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of errors
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+    public function getError(string $input)
+    {
+        if (isset($this->errors[$input])) {
+            foreach ($this->errors[$input] as $error) {
+
+                return $error;
+            }
+        } else
+            return null;
+    }
+
+    /**
+     * Set the value of errors
+     *
+     * @return  self
+     */
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
+
+        return $this;
+    }
+
+    public function required()
+    {
+        if (empty($this->value)) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " is required";
+        }
+        return $this;
+    }
+    public function max(int $max)
+    {
+        if (strlen($this->value) > $max) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " length should less than $max character";
+        }
+        return $this;
+    }
+    public function min(int $min)
+    {
+        if (strlen($this->value) < $min) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " length should greater than $min character";
+        }
+        return $this;
+    }
+    public function in(array $values)
+    {
+        if (!in_array($this->value, $values)) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " must be" . implode(',', $values);
+        }
+        return $this;
+    }
+    public function regex(string $pattern)
+    {
+        if (!preg_match($pattern, $this->value)) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " invalid";
+        }
+        return $this;
+    }
+    public function confirmation($input)
+    {
+        if (!($this->value == $input)) {
+            // array_push($this -> errors, "This field is Required");
+            // return false;
+            $this->errors[$this->input][__FUNCTION__] = str_replace("_", " ", $this->input) . " not confirmed";
+        }
+        return $this;
+    }
+}
--- register.php
+++ register.php
@@ -1,15 +1,29 @@
 <?php
 
-
+use App\Database\Http\Requests\validation;
 
 $title = "Register";
 include "layouts/header.php";
 include "layouts/navbar.php";
 include "layouts/breadcrumb.php";
+$validation = new validation;
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //validation
+
+    $validation->setInput('first_name')->setValue($_POST["first_name"])->required()->min(2)->max(32);
+    $validation->setInput('last_name')->setValue($_POST["last_name"])->required()->min(2)->max(32);
+    $validation->setInput('email')->setValue($_POST["email"])->required()->regex('/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/');
+    $validation->setInput('phone')->setValue($_POST['phone'])->required()->regex('/^01[0125][0-9]{8}$/');
+    $validation->setInput('password')->setValue($_POST['password'])->required()->regex('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/', 'mini 8 chars max 32 ,mini one number , one character , one uppercase letter , one lowercase letter , one specidal char')->confirmation($_POST['password_confirmation']);
+    $validation->setInput('password_confirmation')->setValue($_POST['password_confirmation'])->required();
+    $validation->setInput('gender')->setValue($_POST['gender'])->required()->in(['m', 'f']);
+    if (empty($validation->getErrors())) {
+        // no validation error
+    }
+}
 
 
 
-// no validation error
 // generate verification code
 
 // send email
@@ -31,22 +45,23 @@ include "layouts/breadcrumb.php";
                                 <div class="login-register-form">
                                     <?= $error ?? "" ?>
                                     <form method="post">
-                                        <input type="text" name="first_name" placeholder="First Name" value="">
-
-                                        <input type="text" name="last_name" placeholder="Last Name" value="">
-
-                                        <input type="email" name="email" placeholder="Email Address" value="">
-
-                                        <input type="tel" name="phone" placeholder="Phone" value="">
-
+                                        <input type="text" name="first_name" placeholder="First Name" value="<?= $_POST["first_name"] ?>">
+                                        <?php echo $validation->getError("first_name"); ?>
+                                        <input type="text" name="last_name" placeholder="Last Name" value="<?= $_POST["last_name"] ?>">
+                                        <?php echo $validation->getError("last_name"); ?>
+                                        <input type="email" name="email" placeholder="Email Address" value="<?= $_POST["email"] ?>">
+                                        <?php echo $validation->getError("email"); ?>
+                                        <input type="tel" name="phone" placeholder="Phone" value="<?= $_POST["phone"] ?>">
+                                        <?php echo $validation->getError("phone"); ?>
                                         <input type="password" name="password" placeholder="Password">
-
+                                        <?php echo $validation->getError("password"); ?>
                                         <input type="password" name="password_confirmation" placeholder="Password Confirmation">
-
+                                        <?php echo $validation->getError("password_confirmation"); ?>
                                         <select name="gender" class="form-control my-3" id="">
                                             <option value="m">Male</option>
                                             <option value="f">Female</option>
                                         </select>
+                                        <?php echo $validation->getError("gender"); ?>
 
 
                                         <div class="button-box">
