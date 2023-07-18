db5b987623f800e35f9eabe25eede2bb9f7cb8af
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-07-18 18:54:06 +0300

    cart icon in navbar

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Database\Models\Category;
+use App\Database\Models\Cart;
 
 include "App/Database/Models/SubCatagory.php";
 
@@ -10,8 +11,16 @@ $category = new Category;
 $subcatagory = new SubCategory;
 $result = $category->read()->fetch_all(MYSQLI_ASSOC);
 $subresult = $subcatagory->read()->fetch_all(MYSQLI_ASSOC);
-// print_r($subresult[0]['id']);
-// // print_r($result);
+if (isset($_SESSION['user'])) {
+    $carts = new Cart;
+    $cart_result = $carts->setUsers_id($_SESSION['user']->id)->cartList()->fetch_all(MYSQLI_ASSOC);
+    if ($_GET) {
+        if (isset($_GET['delete'])) {
+            $carts->setUsers_id($_SESSION['user']->id)->setProducts_id($_GET['delete'])->deleteCartItem();
+            header("location:cart-page.php");
+        }
+    }
+}
 ?>
 <header class="header-area gray-bg clearfix">
     <div class="header-bottom">
@@ -38,34 +47,35 @@ $subresult = $subcatagory->read()->fetch_all(MYSQLI_ASSOC);
 
 
                                             ?>
-                                                <li>
-                                                    <ul>
-                                                        <li class="font-weight-bold">
-                                                            <?= ucwords($value['en_name'])  ?></li>
+                                            <li>
+                                                <ul>
+                                                    <li class="font-weight-bold">
+                                                        <?= ucwords($value['en_name'])  ?></li>
 
-                                                        <?php
+                                                    <?php
                                                         foreach ($subresult as $subvalue) {
                                                             if ($subvalue['catigories_id'] == $value['id']) {
                                                         ?>
-                                                                <li><a href="shop.php?subcategory=<?= $subvalue['id'] ?>"><?= $subvalue['en_name'] ?></a>
-                                                                </li>
+                                                    <li><a
+                                                            href="shop.php?subcategory=<?= $subvalue['id'] ?>"><?= $subvalue['en_name'] ?></a>
+                                                    </li>
 
-                                                        <?php
+                                                    <?php
                                                             }
                                                         } ?>
 
-                                                    </ul>
-                                                </li>
+                                                </ul>
+                                            </li>
                                             <?php } ?>
                                         </ul>
                                     </li>
                                     <?php
                                     if (isset($_SESSION['user'])) {
                                     ?>
-                                        <li class="top-hover"><a href="cart-page.php">Cart</a>
-                                        </li>
-                                        <li class="top-hover"><a href="wishlist.php">Favorate</a>
-                                        </li>
+                                    <li class="top-hover"><a href="cart-page.php">Cart</a>
+                                    </li>
+                                    <li class="top-hover"><a href="wishlist.php">Favorate</a>
+                                    </li>
                                     <?php
                                     }
                                     ?>
@@ -79,73 +89,77 @@ $subresult = $subcatagory->read()->fetch_all(MYSQLI_ASSOC);
                         <?php
                         if (isset($_SESSION['user'])) {
                         ?>
-                            <div class="header-currency">
-                                <span class="digit"><?= $_SESSION['user']->first_name ?> <i class="ti-angle-down"></i></span>
-                                <div class="dollar-submenu">
-                                    <ul>
-                                        <li><a href="my-account.php">Profile</a></li>
-                                        <li><a href="logout.php">Logout</a></li>
-                                    </ul>
-                                </div>
+                        <div class="header-currency">
+                            <span class="digit"><?= $_SESSION['user']->first_name ?> <i
+                                    class="ti-angle-down"></i></span>
+                            <div class="dollar-submenu">
+                                <ul>
+                                    <li><a href="my-account.php">Profile</a></li>
+                                    <li><a href="logout.php">Logout</a></li>
+                                </ul>
                             </div>
+                        </div>
                         <?php } else {
                         ?>
-                            <div class="header-currency">
-                                <span class="digit">Welcome <i class="ti-angle-down"></i></span>
-                                <div class="dollar-submenu">
-                                    <ul>
-                                        <li><a href="login.php">Login</a></li>
-                                        <li><a href="register.php">Register</a></li>
+                        <div class="header-currency">
+                            <span class="digit">Welcome <i class="ti-angle-down"></i></span>
+                            <div class="dollar-submenu">
+                                <ul>
+                                    <li><a href="login.php">Login</a></li>
+                                    <li><a href="register.php">Register</a></li>
 
-                                    </ul>
-                                </div>
+                                </ul>
                             </div>
+                        </div>
                         <?php } ?>
+                        <?php
+                            if (isset($_SESSION['user'])) { ?>
                         <div class="header-cart">
                             <a href="#">
                                 <div class="cart-icon">
                                     <i class="ti-shopping-cart"></i>
                                 </div>
                             </a>
+
                             <div class="shopping-cart-content">
+                                <?php
+
+                                    $i = 0;
+                                    foreach ($cart_result as $value) {
+                                        # code...
+                                        $i = $i + $value['price'] * $value['quantity'];
+                                    ?>
                                 <ul>
                                     <li class="single-shopping-cart">
-                                        <div class="shopping-cart-img">
-                                            <a href="#"><img alt="" src="assets/img/cart/cart-1.jpg"></a>
-                                        </div>
+
                                         <div class="shopping-cart-title">
-                                            <h4><a href="#">Phantom Remote </a></h4>
-                                            <h6>Qty: 02</h6>
-                                            <span>$260.00</span>
+                                            <h4><a href="#"><?= $value['en_name'] ?> </a></h4>
+                                            <h6>Qty: <?= $value['quantity'] ?></h6>
+                                            <span>$<?= $value['price'] ?></span>
                                         </div>
                                         <div class="shopping-cart-delete">
-                                            <a href="#"><i class="ion ion-close"></i></a>
+                                            <a href="?delete=<?= $value['products_id'] ?>"><i
+                                                    class="ion ion-close"></i></a>
                                         </div>
                                     </li>
-                                    <li class="single-shopping-cart">
-                                        <div class="shopping-cart-img">
-                                            <a href="#"><img alt="" src="assets/img/cart/cart-2.jpg"></a>
-                                        </div>
-                                        <div class="shopping-cart-title">
-                                            <h4><a href="#">Phantom Remote</a></h4>
-                                            <h6>Qty: 02</h6>
-                                            <span>$260.00</span>
-                                        </div>
-                                        <div class="shopping-cart-delete">
-                                            <a href="#"><i class="ion ion-close"></i></a>
-                                        </div>
-                                    </li>
+
                                 </ul>
+                                <?php }
+                                    ?>
                                 <div class="shopping-cart-total">
-                                    <h4>Shipping : <span>$20.00</span></h4>
-                                    <h4>Total : <span class="shop-total">$260.00</span></h4>
+
+                                    <h4>Shipping : <span>$30</span></h4>
+                                    <h4>Total : <span class="shop-total"><?= $i + 30 ?></span></h4>
                                 </div>
                                 <div class="shopping-cart-btn">
                                     <a href="cart-page.php">view cart</a>
                                     <a href="checkout.php">checkout</a>
                                 </div>
                             </div>
+
                         </div>
+                        <?php
+                            } ?>
                     </div>
                 </div>
             </div>
